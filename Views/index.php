4ee7744145d286cde6442b2210f61cf16375d98f
Jessica Estuqui <126989591+Estuqui@users.noreply.github.com>
2023-08-09 17:04:38 -0300

    att

---
+++ Views/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/Views/CSS/estiloindex.css">
+    <link rel="stylesheet" href="/Views/login.js">
+    <title>Página Inicial</title>
+</head>
+
+<body>
+    <div class="overlay-navigation">
+
+        <nav role="navigation">
+            <ul>
+                <li><a href="#" data-content="The beginning">Home</a></li>
+                <li><a href="#" data-content="Curious?">About</a></li>
+                <li><a href="#" data-content="I got game">Skills</a></li>
+                <li><a href="#" data-content="Only the finest">Works</a></li>
+                <li><a href="#" data-content="Don't hesitate">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+
+    <section class="home">
+        <a href="https://codepen.io/fluxus/pen/gPWxXJ" target="_blank">Click for CSS version</a>
+        <div class="open-overlay">
+            <span class="bar-top"></span>
+            <span class="bar-middle"></span>
+            <span class="bar-bottom"></span>
+        </div>
+    </section>
+</body>
+
+</html>
---
+++ Views/login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./CSS/estilologin.css">
+    <title>Página de Login</title>
+</head>
+
+<body>
+    <div class="box">
+        <div class="img-box">
+            <img src="./img/cadastroLT.png">
+        </div>
+        <div class="form-box">
+            <h2>Faça seu Login</h2>
+            <p> Não tem uma conta? <a href="#"> Cadastre-se </a> </p>
+            <form action="#">
+
+                <div class="input-group">
+                    <label for="email">Usuário</label>
+                    <input type="email" id="email" placeholder="Digite o seu email" required>
+                </div>
+
+                <div class="input-group">
+                    <label for="senha">Senha</label>
+                    <input type="password" id="senha" placeholder="Digite sua senha" required>
+                </div>
+
+                <div class="input-group">
+                    <button>Cadastrar</button>
+                </div>
+
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
